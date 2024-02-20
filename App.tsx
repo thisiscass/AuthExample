ad60e0778c8d0b483febc380e65590fd344b0f14
cass <cassianobruno1997@hotmail.com>
2024-02-20 09:48:46 -0300

    Logic to hide the phone number once confirmation code has sent

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Button,
   SafeAreaView,
@@ -6,29 +6,115 @@ import {
   TextInput,
   View,
 } from 'react-native';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+
+function PhoneSection({confirm, phoneNumber, onConfirm, setPhoneNumber }) {
+
+  if(!!confirm) return ''
+
+  return (
+    <View>
+       <TextInput
+              style={styles.phoneNumber}
+              placeholder='Insert your phone number'
+              onChangeText={phoneNumber => setPhoneNumber(phoneNumber)}
+              defaultValue={phoneNumber}
+            />
+            <Button
+              title='Send SMS'
+              onPress={onConfirm}
+            />
+      </View>  
+  )
+}
+
+function ConfirmationCodeSection({code, styles, confirmCode, setCode }) {
+  return (
+    <View>
+      <TextInput
+        style={styles.confirmationCode}
+        placeholder='Insert the confirmation code'
+        onChangeText={code => setCode(code)}
+        defaultValue={code}
+      />
+      <Button
+        title='Validate'
+        onPress={confirmCode}
+      />
+    </View>
+  )
+}
 
 const App = () => {
+  // If null, no SMS has been sent
+  const [confirm, setConfirm] = useState<FirebaseAuthTypes.ConfirmationResult>();
+
+  // verification code (OTP - One-Time-Passcode)
+  const [code, setCode] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('')
+
+  // Handle login
+  function onAuthStateChanged(user: any) {
+    if (user) {
+      console.log('USER', user)
+    }
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; 
+  }, []);
+
+  async function signInWithPhoneNumber(phoneNumber: string) {
+    if(!phoneNumber) return;
+
+    auth().signInWithPhoneNumber(phoneNumber).then((result) => {
+      setConfirm(result);
+    }).catch((reason) => console.log('error', reason));
+  }
+
+  async function confirmCode() {
+    try {
+      await confirm?.confirm(code);
+    } catch (error) {
+      console.log('Invalid code.');
+    }
+  }
+
+  // if (!confirm) {
+  //   return (
+  //     <Button
+  //       title="Phone Number Sign In"
+  //       onPress={() => signInWithPhoneNumber('+1 650-555-3434')}
+  //     />
+  //   );
+  // }
+
+  const onConfirm = () => {
+    signInWithPhoneNumber(phoneNumber)
+  }
+
+  
   return (
     <SafeAreaView style={styles.container}>
       <View style={{padding: 10}}>
-        <TextInput
-          style={styles.phoneNumber}
-          placeholder='Insert yout phone number'
-          onChangeText={phoneNumber => setPhoneNumber(phoneNumber)}
-          defaultValue={phoneNumber}
+        <PhoneSection 
+          onConfirm={onConfirm}
+          phoneNumber={phoneNumber}
+          setPhoneNumber={setPhoneNumber}
+          confirm={confirm}
         />
-        <Button
-          title='Confirm'
-          onPress={onConfirm}
+
+        <ConfirmationCodeSection 
+          code={code}
+          confirmCode={confirmCode}
+          setCode={setCode}
+          styles={styles}
         />
       </View>
+
     </SafeAreaView>
   );
-}
-
-const onConfirm = () => {
-  console.log('Sending message...')
 }
 
 const styles = StyleSheet.create({
@@ -41,6 +127,12 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginVertical: 8,
     height: 40
+  },
+  confirmationCode: {
+    textAlign: 'center',
+    marginVertical: 8,
+    height: 40,
+    marginTop: 10
   },
   fixToText: {
     flexDirection: 'row',
